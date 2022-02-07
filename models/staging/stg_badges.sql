b76cfd5130315f95d987234c0a04f4a0075ae055
christos.shionis@heygo.com <christos.shionis@heygo.com>
2022-02-07 21:57:56 +0000

    create staging folder, sources.yml and stg_badges

---
+++ models/staging/stg_badges.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from {{ source('stackoverflow', 'badges') }}
+
+ )
+,
+
+transformed as (
+
+    select
+        id          as badge_id,
+        name        as badge_name,
+        date        as badge_name,
+        user_id,
+        class       as badge_class,
+        tag_based   as badge_tag_based,              
+    from source   
+)
+
+select * from transformed;
